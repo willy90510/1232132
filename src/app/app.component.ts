2808a5e148f3a99bba946429f47626c89319c5c7
willy90510 <88758212+willy90510@users.noreply.github.com>
2023-03-24 15:21:05 +0800

    3 files modified, 6 added

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'self03';
+  showArea = 0;
+
+  onClick(number: number) {
+    switch (number) {
+      case 1:
+        this.showArea = 1;
+        break;
+      case 2:
+        this.showArea = 2;
+        break;
+      case 3:
+        this.showArea = 3;
+        break;
+      default:
+        this.showArea = 0;
+        break;
+    }
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
